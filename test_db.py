a17570c47700218ca92d91a2060875beb9cd8cb6
pash <pash.mx@gmail.com>
2026-05-29 19:05:11 -0600

    test: update database test script to print portfolio status results

--- test_db.py
+++ test_db.py
@@ -2,13 +2,20 @@ from dotenv import load_dotenv
 load_dotenv('apps/web/.env')
 from supabase import create_client
 import os
+
 url = os.getenv('NEXT_PUBLIC_SUPABASE_URL')
 key = os.getenv('SUPABASE_SERVICE_ROLE_KEY')
 supabase = create_client(url, key)
+
 from apps.backend.src.mcp_handlers import MCPRequestHandler
+
 handler = MCPRequestHandler(supabase)
 ports = supabase.table('portafolios').select('id, user_id').execute()
 if ports.data:
     pid = ports.data[0]['id']
     uid = ports.data[0]['user_id']
-    handler.get_portfolio_status(pid, uid)
+    print(f"Testing port {pid} for user {uid}")
+    res = handler.get_portfolio_status(pid, uid)
+    print("Result:", res)
+else:
+    print("No portfolios found")
